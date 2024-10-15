e9ca4b64ed47791d28a7bf56af1821539c0c710a
jerelynI <144715038+jerelynI@users.noreply.github.com>
2024-10-15 08:36:25 -0400

    Create CFU8.py

---
+++ CFU8.py
@@ -0,0 +1,9 @@
+#jerelyn
+#10-15-2024
+#pd 1
+#cfu 8 
+delivery = input("Did you order delivery? yes or no?")
+if delivery == "yes":
+    print ("great!")
+if delivery == "no":
+    print ("NO?! So who is cooking?")
